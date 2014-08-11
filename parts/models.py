64f77bca9f93ad396fca20a738b4704bf3e3fdc0
Chris Clark <chrisclark@cindersolutions.com>
2014-08-11 14:57:46 -0700

    updating time zones in settings.py

--- parts/models.py
+++ parts/models.py
@@ -2,14 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+class Workers(models.Model):
+	first = models.CharField(max_length=50)
+	last = models.CharField(max_length=50)
+	role = models.CharField(max_length=25)
+	access = models.CharField(max_length=10)
 
-class Part(models.Model):
-	part_number = models.CharField(max_length=50)
-	package = models.CharField(max_length=25)
-	desciption = models.CharField(max_length=200)
+class Bom(models.Model):
+	name = models.CharField(max_length=50)
 	cost = models.FloatField()
-	quantity = models.IntegerField(default=0)
-	projects = models.ManyToManyField(Project)
+	qty = models.IntegerField(default=0)
+	#need to enter a spot in for bom files to be uploaded per section
+
+class Boards(models.Model):
+	name = models.CharField(max_length=50)
+	#schematic = #need to file entry for this.
+	#board = #file entry on this as well.  
+	rev = models.FloatField()	
 
 class Project(models.Model):
 	worker = models.ManyToManyField(Workers)
@@ -18,23 +27,13 @@ class Project(models.Model):
 	board = models.ManyToManyField(Boards)
 	bom = models.ManyToManyField(Bom)
 
-class Boards(models.Model):
-	name = models.CharField(max_length=50)
-	schematic = #need to file entry for this.
-	board = #file entry on this as well.  
-	rev = models.FloatField()
-
-class Bom(models.Model):
-	name = models.CharField(max_length=50)
+class Part(models.Model):
+	part_number = models.CharField(max_length=50)
+	package = models.CharField(max_length=25)
+	desciption = models.CharField(max_length=200)
 	cost = models.FloatField()
-	qty = models.IntegerField(default=0)
-	#need to enter a spot in for bom files to be uploaded per section
-
-class Workers(models.Model):
-	first = models.CharField(max_length=50)
-	last = models.CharField(max_length=50)
-	role = models.CharField(max_length=25)
-	access = models.CharField(max_length=10)
+	quantity = models.IntegerField(default=0)
+	projects = models.ManyToManyField(Project)
 
 
 	def __unicode__(self):
